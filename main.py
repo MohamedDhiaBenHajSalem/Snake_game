93b4064839e8559eea32e4b8290643811dedd442
mohamed <dhia.hsalem@gmail.com>
2024-05-17 10:55:28 +0200

    adding food class and collisions detection with food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle,Screen
 import time
 from snake import Snake
+from food import Food
 
 screen=Screen()
 screen.setup(width=600, height=600)
@@ -8,6 +9,7 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 peter_the_snake=Snake()
+food_snake=Food()
 
 
 screen.listen()
@@ -19,11 +21,16 @@ screen.onkey(fun=peter_the_snake.right,key="Right")
 
 
 
+
 game_is_on=True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     peter_the_snake.move()
+
+    if peter_the_snake.segments[0].distance(food_snake)<15:
+        food_snake.refresh_food()
+
 
 
 
